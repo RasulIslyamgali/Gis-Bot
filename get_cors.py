c60f0bd54121e1a39e4001b6a4e63ed195f00b70
rasulislyamgali <rasul.islyamgali@gmail.com>
2022-01-27 12:55:26 +0600

    add user agent

--- get_cors.py
+++ get_cors.py
@@ -32,6 +32,9 @@ def get_cors(street: str, home_number: str, proxy: str, city="Алматы"):
     proxyDict = {
         proxy.split(":")[0]: proxy
     }
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
+    }
     response = requests.get(url=url, proxies=proxyDict)
     print(Fore.GREEN, response.status_code)
     if str(response.status_code) == "200":

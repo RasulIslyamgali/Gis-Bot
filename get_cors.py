9f5e405a675b344c6cf63a6676542f3318d6d253
rasulislyamgali <rasul.islyamgali@gmail.com>
2022-01-27 12:58:15 +0600

    add user agent

--- get_cors.py
+++ get_cors.py
@@ -35,7 +35,7 @@ def get_cors(street: str, home_number: str, proxy: str, city="Алматы"):
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
     }
-    response = requests.get(url=url, proxies=proxyDict)
+    response = requests.get(url=url, proxies=proxyDict, headers=headers)
     print(Fore.GREEN, response.status_code)
     if str(response.status_code) == "200":
         content = BeautifulSoup(response.content, "html.parser")
